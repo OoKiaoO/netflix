c471c55424960c36df9c2c893812b4525c042d58
Chiara Campalani <campalanichiara@gmail.com>
2021-08-22 18:00:09 +0900

    created Opt-in Form Component & styles

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import faqsData from '../fixtures/faqs.json';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 
 export function FaqsContainer() {
   return (
@@ -12,6 +12,14 @@ export function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+      <Accordion.Item />
+
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it Now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
