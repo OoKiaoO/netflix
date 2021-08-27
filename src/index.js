13b2f9fb226f6befb501fe5aca7bb313e60dddd1
Chiara Campalani <campalanichiara@gmail.com>
2021-08-27 18:27:41 +0900

    created firebase database & populated it with seed file

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import App from './app';
 // Styles
 import { GlobalStyles } from './global-styles';
 import 'normalize.css';
+// Database seed
+import { firebase } from './lib/firebase.prod';
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById('root')
 );
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,23 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { seedDatabase } from '../seed';
+
+// seed the database
+
+// database config.
+
+const config = {
+  apiKey: 'AIzaSyDMMmIpoblsUkqAnvvAQc2SP-y7Yq7e2ms',
+  authDomain: 'netflix-a34e7.firebaseapp.com',
+  projectId: 'netflix-a34e7',
+  storageBucket: 'netflix-a34e7.appspot.com',
+  messagingSenderId: '383905196367',
+  appId: '1:383905196367:web:9494fee3d2bfb5c99cfd3a',
+};
+
+const firebase = Firebase.initializeApp(config);
+
+// seedDatabase(firebase);
+
+export { firebase };
