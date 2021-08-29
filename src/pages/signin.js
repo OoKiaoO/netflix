0002e2187c67a951618ebe67e6f2eab34435672b
Chiara Campalani <campalanichiara@gmail.com>
2021-08-29 18:39:30 +0900

    created Form reusable component & sigin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+// Containers
+import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
+// Components
+import { Form } from '../components';
 
 function Signin() {
-  return <p>Hello from the Signin!</p>;
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // check form input elements are valid (email & password)
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+    // firebase magic here~
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+            <Form.Text>
+              New to Nefltix? <Form.Link to="/signup">Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
 
 export default Signin;
