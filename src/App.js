165caef916a241e37b7e91de18cf8e02e6cd5526
Chiara Campalani <campalanichiara@gmail.com>
2021-09-02 14:35:35 +0900

    created custom hook to handle user status

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 // Routes
 import * as ROUTES from './constants/routes';
 // Pages
 import { Home, Browse, Signin, Signup } from './pages';
 // Helpers
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+// Custom Hooks
+import { useAuthListener } from './hooks/use-auth-listener';
 
 function App() {
-  const user = {};
+  const { user } = useAuthListener();
+  console.log(user);
+
   return (
     <Router>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
-        <Signin />
-      </IsUserRedirect>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
-        <Signup />
-      </IsUserRedirect>
-      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
-        <Browse />
-      </ProtectedRoute>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} exact>
-        <Home />
-      </IsUserRedirect>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <Signup />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
