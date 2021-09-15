ec96ba4bfb920de6c0317b1a02bfad926541865c
Chiara Campalani <campalanichiara@gmail.com>
2021-09-15 13:03:59 +0900

    added sign in functionality tests

---
+++ cypress/integration/functionality/signup/signup.spec.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+const authUser = require('../../../fixtures/signup-user.json');
+
+describe('The signin page', () => {
+  it('should sign in with email and password', () => {
+    // destructuring the data from the JSON file for easier access
+    const { email, password } = authUser;
+
+    cy.visit('http://localhost:3000/signin');
+    cy.get('[placeholder="Email Address"]').type(email);
+    cy.get('[placeholder="Password"]').type(password);
+    cy.get('[data-testid=sign-in]').click();
+    cy.url().should('eq', 'http://localhost:3000/browse');
+    // add logout
+  });
+});
